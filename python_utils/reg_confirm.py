71e6802b8160d68ac83d64c6c7a91cf78e576e11
Shay Palachy <shaypal5@gmail.com>
2017-10-16 15:24:12 +0300

    registration form fixes

--- python_utils/reg_confirm.py
+++
@@ -1,6 +0,0 @@
-"""A script that sends registration confirmation emails."""
-
-from mongo_utils import send_confirmation_emails
-
-if __name__ == "__main__":
-    send_confirmation_emails()
--- python_utils/reg_confirm_status.py
+++
@@ -1,6 +0,0 @@
-"""A script that sends registration confirmation emails."""
-
-from mongo_utils import _print_email_stats
-
-if __name__ == "__main__":
-    _print_email_stats()
