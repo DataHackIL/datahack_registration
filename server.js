d54b47309bc5958d15d8b862643ae8906b2bcc06
shakedlokits <shaked.lokits@gmail.com>
2019-07-17 00:34:15 +0300

    Added acme test

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
+const https = require('https')
+const fs = require('fs');
+
 require('dotenv').config()
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 var config = require('./config/config'),
-	mongoose = require('./config/mongoose'),
-	express = require('./config/express'),
-	passport = require('./config/passport');
+    mongoose = require('./config/mongoose'),
+    express = require('./config/express'),
+    passport = require('./config/passport')
 
 var db = mongoose(),
-	app = express(),
-	passport = passport();
+    app = express(),
+    passport = passport()
+
+// options = {
+//     key: fs.readFileSync('./key.pem'),
+//     cert: fs.readFileSync('./cert.pem'),
+//     passphrase: 'SuperSecret'
+// }
 
 app.listen(config.port);
+// https.createServer(options, app).listen(8443)
 
-module.exports = app;
-console.log(process.env.NODE_ENV + ' server running at http://' + config.host + ':' + config.port);
+module.exports = app
+console.log(process.env.NODE_ENV + ' server running at http://' + config.host + ':' + config.port)
